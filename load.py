2f11b632ff40719de6b80a46e004e3b23076ce9b
WillemvdVelden <willem.vandervelden1995@gmail.com>
2017-11-03 11:24:26 +0100

    csv added

--- load.py
+++ load.py
@@ -1,10 +1,10 @@
 import csv
 import numpy
 
-reader = csv.reader(open('ConnectiesHolland.csv', 'rb'), delimiter = ',')
-x = list(reader)
-ConnectiesHolland = numpy.array(x).astype("string")
-print ConnectiesHolland
+# reader = csv.reader(open('ConnectiesHolland.csv', 'rb'), delimiter = ',')
+# x = list(reader)
+# ConnectiesHolland = numpy.array(x).astype("string")
+# print ConnectiesHolland
 
 reader = csv.reader(open('StationsHolland.csv', 'rb'), delimiter = ',')
 x = list(reader)
