9a94307e0fe2f3a9b62a836d69e47b1979e11f71
WillemvdVelden <willem.vandervelden1995@gmail.com>
2017-11-03 11:47:37 +0100

    load.py working

--- load.py
+++ load.py
@@ -6,7 +6,16 @@ import numpy
 # ConnectiesHolland = numpy.array(x).astype("string")
 # print ConnectiesHolland
 
+# read csv file from directory
 reader = csv.reader(open('StationsHolland.csv', 'rb'), delimiter = ',')
 x = list(reader)
 StationsHolland = numpy.array(x).astype("string")
-print StationsHolland
+
+# ititiate new list with stations
+station_list = []
+
+# iterate over list and append station names to list
+for lijst in StationsHolland:
+	station_list.append(lijst[0])
+
+print station_list
