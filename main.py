13471a8b556171d01cb7de8f97844f7363c192c7
[JasperNaberman] <jaspernaberman@live.nl>
2017-11-06 12:59:06 +0100

    graph with networkx

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import csv
 import numpy
 from helpers import *
 from graph import *
+import networkx as nx
 
 def main():
     # intitialize graph
@@ -22,8 +23,20 @@ def main():
     for connection in ConnectionsHolland:
     	graph.add_connection({connection[0], connection[1]})
 
-    # print the graph
-    print(g)
+    # make new graph with package nx
+    test = nx.Graph()
+    
+    for station in StationsHolland:
+    	test.add_node(station[0])
+        
+    for connection in ConnectionsHolland:
+    	test.add_edge(connection[0], connection[1])
+    
+        
+    print test.nodes()
+    print test.edges()
+    
+    
 
 # call the main-function
 if __name__ == "__main__":
