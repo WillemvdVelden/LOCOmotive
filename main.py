15886e217cdc77d8971537b6dc06dab1133280f1
mannus schomaker <mannusschomaker@MBP-van-mannus.fritz.box>
2017-11-09 01:30:06 +0100

    attributten toegevoegt en hij tekend nu ook alles goed

--- main.py
+++ main.py
@@ -30,26 +30,49 @@ def main():
 
     # make new graph with package nx
     G = nx.Graph()
-    
+    k = []
+    n = []
     for station in StationsHolland:
-    	G.add_node(station[0])
-        
-    for connection in ConnectionsHolland:
-    	G.add_edge(connection[0], connection[1], weight = connection[2])
-        
-    elarge=[(u,v) for (u,v,d) in G.edges(data = True) if d['weight'] > 0.5]
-    esmall=[(u,v) for (u,v,d) in G.edges(data = True) if d['weight'] <= 0.5]
+        if station[3]:
+            G.add_node(station[0], pos = (float(station[1]),float(station[2])) ,type = 'Kritiek')
+            k.append(station[0])
+        else:
+            G.add_node(station[0], pos = (float(station[1]),float(station[2])) ,type = 'not')
+            n.append(station[0])
+
+    print(k)
+    print(g)
     
+    for connection in ConnectionsHolland:
+        if (connection[0] in k) or (connection[1] in k):
+            G.add_edge(connection[0], connection[1], weight = connection[2], type = 'Kritiek')
+        else:
+            G.add_edge(connection[0], connection[1], weight = connection[2], type = 'not')
+        
+    elarge=[(u,v) for (u,v,d) in G.edges(data = True) if d['type'] == 'Kritiek']
+    esmall=[(u,v) for (u,v,d) in G.edges(data = True) if d['type'] == 'not']
+    
+    pos=nx.get_node_attributes(G,'pos')
+    print(pos)
     # positions for all nodes
-    pos = nx.spring_layout(G)
     # nodes
-    nx.draw_networkx_nodes(G, pos, node_size = 100)
+    nx.draw_networkx_nodes(G,pos,
+                       nodelist=k,
+                       node_color='r',
+                       node_size=150,
+                   alpha=0.8)
+
+    nx.draw_networkx_nodes(G,pos,
+                       nodelist=n,
+                       node_color='b',
+                       node_size=50,
+                   alpha=0.8)
     
     # edges
     nx.draw_networkx_edges(G, pos, edgelist = elarge,
                         width = 2)
     nx.draw_networkx_edges(G,pos,edgelist = esmall,
-                        width = 2,alpha = 0.5,edge_color = 'b',style = 'dashed')
+                        width = 2, alpha = 0.5 ,edge_color = 'b',style = 'dashed')
                         
     # labels
     nx.draw_networkx_labels(G, pos, font_size = 8, font_family = 'sans-serif')
