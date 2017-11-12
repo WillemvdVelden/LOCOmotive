912eab75ba6eb0943b335d84e1957e8864da45b1
[JasperNaberman] <jaspernaberman@live.nl>
2017-11-12 21:50:27 +0100

    Added Train class and updateTrain-functionality.

--- helpers.py
+++ helpers.py
@@ -1,9 +1,72 @@
 import csv
 import numpy
+import networkx as nx
+from main import *
+
+try:
+    import matplotlib.pyplot as plt
+except:
+    raise
 
 # read csv file from directory for vertices
-def csv_reader(csv_name):
+def csvReader(csv_name):
     reader = csv.reader(open(csv_name, 'rb'), delimiter = ',')
-    x = list(reader)
-    array = numpy.array(x).astype("string")
+    lReader = list(reader)
+    array = numpy.array(lReader).astype('string')
     return array
+
+# plot the railway map with all it's attributes
+def draw(graph, c, n):
+	eLarge = [(u, v) for (u, v, d) in graph.edges(data = True) if d['type'] == 'critical']
+	eSmall = [(u, v) for (u, v, d) in graph.edges(data = True) if d['type'] == 'non-critical']
+
+	# positions for all nodes
+	pos = nx.get_node_attributes(graph, 'pos')
+    
+    # edge labels
+	edgeLabels = nx.get_edge_attributes(graph, 'weight')
+
+	# nodes
+	nx.draw_networkx_nodes(graph, pos, nodelist = c, node_color = 'r', node_size = 150, alpha = 0.8)
+	nx.draw_networkx_nodes(graph, pos, nodelist = n, node_color = 'b', node_size = 50, alpha = 0.8)
+
+	# edges
+	nx.draw_networkx_edges(graph, pos, edgelist = eLarge, width = 2)
+	nx.draw_networkx_edges(graph, pos, edgelist = eSmall,
+	                    width = 2, alpha = 0.5, edge_color = 'g', style = 'dashed')
+	                    
+	# labels
+	nx.draw_networkx_labels(graph, pos, font_size = 8, font_family = 'sans-serif')
+    
+	nx.draw_networkx_edge_labels(graph, pos, edge_labels = edgeLabels, font_size = 8, font_family = 'sans-serif')
+        
+    # display
+	plt.show()
+    
+# define a class for trains
+class Train:
+    currentLocation = ''
+    previousLocation = ''
+    traveledTime = 0
+    
+# update a train's attributes
+def updateTrain(trainName, newLocation, allStations, graph):
+    # check if it's the first station the train attends
+    if trainName.currentLocation == '':
+        trainName.currentLocation = newLocation
+    else:         
+        # retrieve the station indexes
+        curIndex = allStations.index(trainName.currentLocation)
+        newIndex = allStations.index(newLocation)
+        
+        # compute the traveld time with edge attributes
+        minutes = graph[allStations[curIndex]][allStations[newIndex]]['weight']
+        trainName.traveledTime += minutes.astype(numpy.int)
+        
+        # update the train's location attributes
+        trainName.previousLocation = trainName.currentLocation
+        trainName.currentLocation = newLocation
+    
+    
+    
+    
--- main.py
+++ main.py
@@ -2,40 +2,48 @@ import csv
 import numpy
 import networkx as nx
 from helpers import *
-from draw import *
 
 
 def main():
-    # read csv file with csv_reader from helpers.py
-    StationsHolland = csv_reader('StationsHolland.csv')
-
-    # read csv file with csv_reader from helpers.py
-    ConnectionsHolland = csv_reader('ConnectiesHolland.csv')
+    # read .csv-files with the csv_reader()-function
+    StationsHolland = csvReader('StationsHolland.csv')
+    ConnectionsHolland = csvReader('ConnectiesHolland.csv')
 
     # initialize graph with package nx
     graph = nx.Graph()
     c = []
     n = []
+    allStations = []
+    
+    # add all stations and attributes to the graph as nodes
     for station in StationsHolland:
+        allStations.append(station[0])
         if station[3]:
-            graph.add_node(station[0], pos = (float(station[1]),float(station[2])), type = 'critical')
+            graph.add_node(station[0], pos = (float(station[2]), float(station[1])), type = 'critical')
             c.append(station[0])
         else:
-            graph.add_node(station[0], pos = (float(station[1]),float(station[2])) ,type = 'non-critical')
+            graph.add_node(station[0], pos = (float(station[2]), float(station[1])), type = 'non-critical')
             n.append(station[0])
-
-    print(c)
-    print(graph)
     
+    # add all connections and attributes to the graph as edges
     for connection in ConnectionsHolland:
         if (connection[0] in c) or (connection[1] in c):
             graph.add_edge(connection[0], connection[1], weight = connection[2], type = 'critical')
         else:
             graph.add_edge(connection[0], connection[1], weight = connection[2], type = 'non-critical')
         
-    draw(graph, c, n)
+    # plot the railway map with all it's attributes
 
     
+    # initialize an object of class Train
+    train1 = Train()
+    print(train1.currentLocation, train1.previousLocation, train1.traveledTime)
+
+    updateTrain(train1, 'Amsterdam Centraal', allStations, graph)
+    updateTrain(train1, 'Amsterdam Amstel', allStations, graph)
+    updateTrain(train1, 'Amsterdam Zuid', allStations, graph)
+    
+    print(train1.currentLocation, train1.previousLocation, train1.traveledTime)
 
 # call the main-function
 if __name__ == "__main__":
