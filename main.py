f8c59ace683671e55c541cd9a24f699442c4894c
mannus schomaker <mannusschomaker@wcw-stud-145-109-15-112.wireless.uva.nl>
2017-11-14 15:11:29 +0100

    path

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import numpy
 import networkx as nx
 from helpers import *
 from visualize import *
+from pseudo import *
+# type value 0 is critical type value 1 is non critical
 
 
 def main():
@@ -20,38 +22,45 @@ def main():
     for station in stations_holland:
         all_stations.append(station[0])
         if station[3]:
-            graph.add_node(station[0], pos = (float(station[2]), float(station[1])), type = 'critical')
+            graph.add_node(station[0], pos = (float(station[2]), float(station[1])), type = 0)
             criticals.append(station[0])
         else:
-            graph.add_node(station[0], pos = (float(station[2]), float(station[1])), type = 'non-critical')
+            graph.add_node(station[0], pos = (float(station[2]), float(station[1])), type = 1)
             non_criticals.append(station[0])
     
     # add all connections and attributes to the graph as edges
     for connection in connections_holland:
         if (connection[0] in criticals) or (connection[1] in criticals):
-            graph.add_edge(connection[0], connection[1], weight = connection[2], type = 'critical')
+            graph.add_edge(connection[0], connection[1], weight = connection[2], type = 0)
         else:
-            graph.add_edge(connection[0], connection[1], weight = connection[2], type = 'non-critical')
-        
-    # plot the railway map with all it's attributes
-    draw(graph, criticals, non_criticals)
+            graph.add_edge(connection[0], connection[1], weight = connection[2], type = 1)
     
-############## EXAMPLE #############    
-    # initialize an object of class Train (see helpers.py)
-    train_1 = Train()
-    
-    # see how it looks like directly after creation
-    print(train_1.current_location, train_1.previous_location, train_1.traveled_time)
-    
-    # let it start at centraal, go to amstel, and then to zuid (see helpers.py)
-    update_train(train_1, 'Amsterdam Centraal', all_stations, graph)
-    update_train(train_1, 'Amsterdam Amstel', all_stations, graph)
-    update_train(train_1, 'Amsterdam Zuid', all_stations, graph)
-    
-    # see how it looks like now
-    print(train_1.current_location, train_1.previous_location, train_1.traveled_time)
-############## EXAMPLE END #############
 
+    came_from, cost_so_far = dijkstra_search(graph, 'Den Haag Centraal', 'Schiedam Centrum')
+
+    path = reconstruct_path(came_from, 'Den Haag Centraal', 'Schiedam Centrum')
+    print(path)
+    # plot the railway map with all it's attributes
+    # draw(graph, criticals, non_criticals)
+    
+# ############## EXAMPLE #############    
+#     # initialize an object of class Train (see helpers.py)
+#     train_1 = Train()
+    
+#     # see how it looks like directly after creation
+#     print(train_1.current_location, train_1.previous_location, train_1.traveled_time)
+    
+#     # let it start at centraal, go to amstel, and then to zuid (see helpers.py)
+#     update_train(train_1, 'Amsterdam Centraal', all_stations, graph)
+#     update_train(train_1, 'Amsterdam Amstel', all_stations, graph)
+#     update_train(train_1, 'Amsterdam Zuid', all_stations, graph)
+    
+#     # see how it looks like now
+#     print(train_1.current_location, train_1.previous_location, train_1.traveled_time)
+# ############## EXAMPLE END #############
+
+
+# heel path op non critical
 # call the main-function
 if __name__ == "__main__":
     main()
--- pseudo.py
+++ pseudo.py
@@ -1,5 +1,52 @@
-procedure DFS(graph, station):
-    label station as discovered
-    for all edges from station to station2 in G.adjacentEdges(station) do
-        if edge is not labeled as discovered then
-            recursively call DFS(graph, station2)
+import heapq
+import numpy
+
+class PriorityQueue:
+    def __init__(self):
+        self.elements = []
+    
+    def empty(self):
+        return len(self.elements) == 0
+    
+    def put(self, item, priority):
+        heapq.heappush(self.elements, (priority, item))
+    
+    def get(self):
+        return heapq.heappop(self.elements)[1]
+
+def dijkstra_search(graph, start, goal):
+    frontier = PriorityQueue()
+    frontier.put(start, 0)
+    came_from = {}
+    cost_so_far = {}
+    came_from[start] = None
+    cost_so_far[start] = 0
+    current_time = {}
+    
+    while not frontier.empty():
+        current = frontier.get()
+        
+        if current == goal:
+            break
+        
+        for next in graph.neighbors(current):
+            new_cost = cost_so_far[current] + graph[current][next]['type']
+            if next not in cost_so_far or new_cost < cost_so_far[next]:
+                cost_so_far[next] = new_cost
+                print(cost_so_far)
+                priority = new_cost
+                print(priority)
+                frontier.put(next, priority)
+                came_from[next] = current
+    
+    return came_from, cost_so_far
+
+def reconstruct_path(came_from, start, goal):
+    current = goal
+    path = []
+    while current != start:
+        path.append(current)
+        current = came_from[current]
+    path.append(start) # optional
+    path.reverse() # optional
+    return path
